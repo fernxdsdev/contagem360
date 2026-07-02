29daad9037d44b52398fd1131e2d3bbd8d8bf822
samambaiabr <teamomae231@gmail.com>
2026-07-01 21:32:22 -0300

    CADSTRO 100%COMPLETO

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const FormLogin = document.getElementById('FormLogin');
 
-FormLogin.addEventListener('submit', function (event){
+FormLogin.addEventListener('submit', function (event) {
 
     event.preventDefault();
 
@@ -15,25 +15,31 @@ FormLogin.addEventListener('submit', function (event){
 
         email.classList.add("is-invalid");
         email.classList.remove("is-valid");
-    
+
     } else if (!email.checkValidity()) {
-    
+
         email.classList.add("is-invalid");
         email.classList.remove("is-valid");
-    
+
     } else {
-    
+
         email.classList.add("is-valid");
         email.classList.remove("is-invalid");
-    
+
     }
     if (senha.value.trim() == "") {
-    
+
         senha.classList.add("is-invalid")
         senha.classList.remove("is-valid")
     } else {
-    
+
         senha.classList.add("is-valid");
         senha.classList.remove("is-invalid")
     }
-});
+
+
+    if (email.value.trim() == "a@a.com" && senha.value.trim() == "123456") {
+        window.location.href = "eventos.html";
+      
+    }
+})
